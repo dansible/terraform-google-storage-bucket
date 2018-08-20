2e7b23a888265b0206545d60e33f69353b9b2f34
Daniel Pacrami <daniel.pacrami@sap.com>
2018-08-20 09:44:17 -0400

    Expose outputs as single values

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "name" {
-  value       = ["${google_storage_bucket.bucket.name}"]
+  value       = "${google_storage_bucket.bucket.name}"
   description = "The name of bucket."
 }
 
 output "self_link" {
-  value       = ["${google_storage_bucket.bucket.self_link}"]
+  value       = "${google_storage_bucket.bucket.self_link}"
   description = "The URI of the created resource."
 }
 
 output "url" {
-  value       = ["${google_storage_bucket.bucket.url}"]
+  value       = "${google_storage_bucket.bucket.url}"
   description = "The base URL of the bucket, in the format gs://<bucket-name>."
 }
